c2ddc5762cffd09ae7853f6e912318fbe83db3f1
KimGiWoon <rldns20704@naver.com>
2025-04-11 09:59:00 +0900

    [Feat] Test Code
    테스트 코드 추가

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -1,12 +1,27 @@
-﻿namespace GitStudy
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GitStudy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("추가함!");
-            Console.WriteLine("이것도 추가함!");
+            int[] answer = new int[] {1, 3, 4, 5, 7, 11, 22, 44, 32 };
+            int[] value = new int[answer.Length];
+            for (int i = 0; i < answer.Length; i++)
+            {
+
+                if (answer[i] % 2 == 1)
+                {
+                    value[i] = answer[i];
+                }
+            }
+            Array.Sort(value);
+            answer = value;
+            
+            Console.WriteLine(answer);
+
+
         }
     }
 }
